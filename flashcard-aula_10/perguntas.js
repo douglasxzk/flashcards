3b9a6f437357292634e2ce3f1c3c50d51f58d142
douglasxzk <149505507+douglasxzk@users.noreply.github.com>
2024-09-29 20:14:42 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'bilheteira',
+    'Qual foi o filme de terror com a maior bilheteira? ',
+    'A resposta é “It: A Coisa", arrecadando mais de 700 milhões de reais.'
+)
+
+criaCartao(
+    'Oscar',
+    'Qual filme ganhou o Oscar de Melhor Filme em 2017?',
+    '"Moonlight", embora "La La Land" tenha ganhado inicialmente, eles corrigiram.'
+)
+
+criaCartao(
+    'Sexta-Feira 13',
+    'Quem é o assassino em "Sexta-Feira 13" (1980)?',
+    'Embora muitos achem que é o Jason, não é. É a mãe dele, Pamela Voorhees. O Jason aparece apenas no segundo filme.'
+)
+
+criaCartao(
+    'Johnny Depp',
+    'Qual filme de terror foi a estreia de Johnny Depp?',
+    '“A Hora do Pesadelo", de 1984.'
+)
